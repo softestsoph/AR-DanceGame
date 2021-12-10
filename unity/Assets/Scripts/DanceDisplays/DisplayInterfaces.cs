a81d60835037a41ef63aeb2fb95fefc18a455536
YellowTech <oliver.fcst@hotmail.com>
2021-12-10 01:41:43 +0100

    Added GoalGhostDisplay, enabled danceData as goal input, some material and vfx property updates and prefabs

--- unity/Assets/Scripts/DanceDisplays/DisplayInterfaces.cs
+++ unity/Assets/Scripts/DanceDisplays/DisplayInterfaces.cs
@@ -6,12 +6,17 @@ namespace PoseTeacher {
     public interface IAvatarDisplay {
         public void SetPose(PoseData pose);
 
+        public void FadeOut();
+
         public GameObject gameObject { get; }
 
     }
 
     public interface IGoalDisplay {
         public void showGoal(PoseData pose, float alpha);
+
+        public void showDanceData(DanceData data, float goalStartTime);
+
         public void showNothing();
 
         public GameObject gameObject { get; }
